a42cd3f455956fb71a788edbd6edc6e9a5c29405
Pattabhi43 <kpramaiah46@gmail.com>
2023-03-30 02:23:40 +0530

    Made the sequence change in vpc

--- Modules/modules/vpc-04/main.tf
+++ Modules/modules/vpc-04/main.tf
@@ -1,11 +1,11 @@
-resource "aws_vpc" "vpc-04" {
+resource "aws_vpc" "vpc-03" {
     cidr_block = var.cidr_block
     tags = {
       "Name" = "vpc-04"
     }
 }
 
-resource "aws_subnet" "pub-01" {
+resource "aws_subnet" "pub-02" {
     cidr_block = var.sub-01
     tags = {
       "Name" = "subnet-01"
